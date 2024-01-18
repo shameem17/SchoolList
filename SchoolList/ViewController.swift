e491a2f215c461951c085f587e72522521c6f3f6
shameem17 <shameem105455@gmail.com>
2024-01-18 15:00:01 +0600

    Initial Commit

---
+++ SchoolList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolList
+//
+//  Created by Shameem on 18/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
